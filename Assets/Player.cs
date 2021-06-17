46d80ce2595b9947a559fa3d0cffdb37647126f3
Sakref00unholy <phillipmayland@gmail.com>
2021-06-17 13:49:14 +0200

    first (or third) commit
    this is technically my third commit, but due to a spelling error in the git file i had to remake the repository. this is a Classic RPG style game project. i've added a inventory system with items you can pick up, move in the inventory,  save it and even destroy items. i've also added a character that can walk around and pick items up.

---
+++ Assets/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public InventoryObject inventory;
+    
+    public void OnTriggerEnter(Collider other)
+    {
+        var item = other.GetComponent<GroundItem>();
+        if (item)
+        {
+            inventory.AddItem(new Item (item.item), 1);
+            Destroy(other.gameObject);
+
+        }
+    }
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            inventory.Save();
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            inventory.Load();
+        }
+    }
+    private void OnApplicationQuit()
+    {
+        inventory.Container.Items = new InventorySlot[42];
+    }
+}
